53c94cb9647ed36e899e299134aa7f245655481f
Ernst-Paul <epswens@gmail.com>
2023-12-14 23:21:15 +0100

    add ask_spoon

--- R/ask_spoon.R
+++ R/ask_spoon.R
@@ -11,8 +11,32 @@
 #' \dontrun{ask_spoon("linearity")}
 ask_spoon <- function(spoon) {
   yaml <- yamls[yamls$spoon == spoon,]
+  # add type control character scalar
+  if (nrow(yaml) == 0) {
+    stop(paste("no spoon matches", spoon))
+  }
 
-  # cat(stringr::str_trunc(yaml$desc, 30, ellipsis = "\U2026"))
-  yaml
-  NULL
+  cat("id:", yaml$id, "\n")
+  cat("title:", yaml$title, "\n")
+  cat("description:", yaml$desc, "\n")
+  cat("category:", yaml$category, "\n")
+  cat("classes:", paste(yaml$classes[[1]], collapse = ", "), "\n")
+  cat("parameters:", paste(names(yaml$params[[1]]), collapse = ", "), "\n")
+  for (para in names(yaml$params[[1]])) {
+    cat(" ", para)
+    para_value = yaml$params[[1]][[para]]
+    if (!is.null(para_value)) {
+      cat(" =", para_value)
+    }
+    para_desc = yaml$params_desc[[1]][[para]]
+    if (!is.null(para_desc)) {
+      cat(",", para_desc)
+    }
+    cat("\n")
+  }
+  auth_names <- sapply(yaml$authors[[1]], function(x) x$name)
+  cat("authors:", paste(auth_names, collapse = ", "), "\n")
+  cat("format:", paste(yaml$formats[[1]], collapse = ", "))
+
+  invisible(yaml)
 }
